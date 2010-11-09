7ec250d5076a1df2c595e747e2bf8d8923b5eb0c
Nick Lang <nicholas@nick-langs-macbook-pro-44.local>
2010-11-08 23:38:04 -0600

    changing urls to use custom edit profile view

--- runkansas/profiles/urls.py
+++ runkansas/profiles/urls.py
@@ -3,7 +3,8 @@ from idios.urls import urlpatterns as idios_urls
 
 
 runks_patterns = patterns("",
+    url(r"^edit/$", "runkansas.profiles.views.profile_edit", name="profile_edit"),
     url(r"^profile/(?P<username>[\w\._-]+)/ical/$", "runkansas.profiles.views.my_ical", name="my_ical"),
 )
 
-urlpatterns = idios_urls + runks_patterns
+urlpatterns = runks_patterns + idios_urls
