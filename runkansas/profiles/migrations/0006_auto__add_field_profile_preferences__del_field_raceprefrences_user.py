daaedf71fb44c83fd908b38a67cf502df8074d60
Nick Lang <nicholas@nick-langs-macbook-pro-44.local>
2010-11-09 00:06:14 -0600

    trying to get racer prefernes working right

---
+++ runkansas/profiles/migrations/0006_auto__add_field_profile_preferences__del_field_raceprefrences_user.py
@@ -0,0 +1,120 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding field 'Profile.preferences'
+        db.add_column('profiles_profile', 'preferences', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['profiles.RacePrefrences'], null=True, blank=True), keep_default=False)
+
+        # Deleting field 'RacePrefrences.user'
+        db.delete_column('profiles_raceprefrences', 'user_id')
+
+
+    def backwards(self, orm):
+        
+        # Deleting field 'Profile.preferences'
+        db.delete_column('profiles_profile', 'preferences_id')
+
+        # We cannot add back in field 'RacePrefrences.user'
+        raise RuntimeError(
+            "Cannot reverse this migration. 'RacePrefrences.user' and its values cannot be restored.")
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'profiles.profile': {
+            'Meta': {'object_name': 'Profile'},
+            'about': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'location': ('django.db.models.fields.CharField', [], {'max_length': '40', 'null': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'preferences': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['profiles.RacePrefrences']", 'null': 'True', 'blank': 'True'}),
+            'races': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['race.Event']", 'null': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'unique': 'True'}),
+            'website': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'})
+        },
+        'profiles.raceprefrences': {
+            'Meta': {'object_name': 'RacePrefrences'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'max_distance': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'max_distance'", 'to': "orm['race.Distance']"}),
+            'min_distance': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'min_distance'", 'to': "orm['race.Distance']"}),
+            'types': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['race.RaceType']", 'symmetrical': 'False'})
+        },
+        'race.distance': {
+            'Meta': {'object_name': 'Distance'},
+            'distance': ('django.db.models.fields.DecimalField', [], {'max_digits': '10', 'decimal_places': '1'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '40'}),
+            'unit': ('django.db.models.fields.IntegerField', [], {})
+        },
+        'race.event': {
+            'Meta': {'object_name': 'Event'},
+            'date': ('django.db.models.fields.DateTimeField', [], {}),
+            'distance': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['race.Distance']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'race': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['race.Race']"})
+        },
+        'race.race': {
+            'Meta': {'object_name': 'Race'},
+            'contact_email': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'contact_name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'contact_phone': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'date': ('django.db.models.fields.DateField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'location': ('django.db.models.fields.TextField', [], {}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'race_type': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'type'", 'to': "orm['race.RaceType']"}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'db_index': 'True'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'})
+        },
+        'race.racetype': {
+            'Meta': {'object_name': 'RaceType'},
+            'discipline': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'db_index': 'True'}),
+            'terrain': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['profiles']
