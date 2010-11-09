e177454c3f85a685d01af3316139091e7f0cbdc5
Nick Lang <nicholas@nick-langs-macbook-pro-44.local>
2010-11-08 23:38:29 -0600

    using custom view to utilize custom form

--- runkansas/profiles/views.py
+++ runkansas/profiles/views.py
@@ -11,7 +11,46 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
 from runkansas.profiles.feeds import MyRaceCalendar
+from runkansas.profiles.forms import ProfileForm
+from idios.views import group_and_bridge, group_context
 
+@login_required
+def profile_edit(request, **kwargs):
+    """
+    profile_edit
+    """
+    template_name = kwargs.pop("template_name", "idios/profile_edit.html")
+    form_class = kwargs.pop("form_class", None)
+    
+    
+    if request.is_ajax():
+        template_name = kwargs.get(
+            "template_name_facebox",
+            "idios/profile_edit_facebox.html"
+        )
+    
+    group, bridge = group_and_bridge(kwargs)
+    
+    # @@@ not group-aware (need to look at moving to profile model)
+    profile = request.user.get_profile()
+    
+    if request.method == "POST":
+        profile_form = ProfileForm(request.POST, instance=profile)
+        if profile_form.is_valid():
+            profile = profile_form.save(commit=False)
+            profile.user = request.user
+            profile.save()
+            return HttpResponseRedirect(profile.get_absolute_url(group=group))
+    else:
+        profile_form = ProfileForm(instance=profile)
+    
+    ctx = group_context(group, bridge)
+    ctx.update({
+        "profile": profile,
+        "profile_form": profile_form,
+    })
+    
+    return render_to_response(template_name, RequestContext(request, ctx))
 
 
 @login_required
